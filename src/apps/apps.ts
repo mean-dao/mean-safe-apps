ff9ef58128eb9b60cd00d090e8f3a1a366462119
Yansel <yanselfgok@gmail.com>
2022-05-09 19:41:31 -0400

    Change `apps.json` structure and UI Element types. Updating MSP definition.

--- src/apps/apps.ts
+++ src/apps/apps.ts
@@ -1,4 +1,4 @@
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { fetch } from "cross-fetch";
 import data from './apps.json';
 
@@ -8,51 +8,56 @@ export enum NETWORK {
   Devnet = 103,
 }
 
-export type UiType =
-  | "Boolean"
-  | "Enum"
-  | "Small"
-  | "Number"
-  | "Big"
-  | "Float"
-  | "String"
-  | "PublicKey"
-  | "Date"
-  | "Object"
-  | "Array";
+export type UiElementVisibility = "show" | "hide" | "readOnly";
 
-export interface App {
+export type UiType = 
+    "yesOrNo"
+  | "option"
+  | "optionOwners"
+  | "optionAccounts"
+  | "inputText"
+  | "inputTextArea"
+  | "inputNumber"
+  | "datePicker"
+  | "slot"
+  | "knownValue"
+  | "treasuryAccount"
+  | "txProposer";
+
+export type App = {
   id: string;
   name: string;
   network: NETWORK;
   logoUri: string;
-  instructions: Instruction[];
+  definition: any;
+  uiElements: UiElement[];
 }
 
-export interface Instruction {
-  id: string;
+export type UiElement = {
+  id: string,
   name: string;
   label: string;
-  args: Arg[];
-  accounts: Account[];
-  remainingAccounts: Account[];
+  help: string;
+  type: UiType;
+  value: any;
+  visibility: UiElementVisibility,
+  dataElement: DataElement | undefined,
 }
 
-export interface Account {
+export type Account = {
   index: number;
   name: string;
-  label: string;
   isSigner: boolean;
   isWritable: boolean;
 }
 
-export interface Arg {
+export type Arg = {
   position: number;
   name: string;
-  label: string;
-  uiType: string;
-  dataType: string;
+  dataType: any;
 }
+
+export type DataElement = Account | Arg;
 
 export class AppsProvider {
 
@@ -82,93 +87,73 @@ const getApps = (network?: NETWORK) => {
 
 const parseApp = async (data: any): Promise<App | null> => {
   try {
+    const response = await fetch(data.definition);
+    const result = (await response.json()) as any;
     let program = {
       id: data.id,
       name: data.name,
       network: data.network,
       logoUri: data.logo,
-      instructions: []
+      definition: result,
+      uiElements: []
     } as App;
-    const response = await fetch(data.definition);
-    const result = (await response.json()) as any;
-    let instructions: Instruction[] = [];
+    let uiElements: UiElement[] = [];
     for (let uiIx of data.instructions) {
       const dataIx = result.instructions.filter((ix: any) => ix.name === uiIx.name)[0];
       if (dataIx) {
         const ix = await parseInstruction(program.id, uiIx, dataIx);
         if (ix) {
-          instructions.push(ix);
+          uiElements.push(ix);
         }
       }
     }
-    program.instructions = instructions;
+    program.uiElements = uiElements;
     return program;  
   } catch {
     return null;
   }
 };
 
-const parseInstruction = async (programId: string, uiIx: any, dataIx: any): Promise<Instruction | null> => {
+const parseInstruction = async (programId: string, uiIx: any, dataIx: any): Promise<UiElement | null> => {
   try {
-    let instruction = {
-      id: (await PublicKey.findProgramAddress(
-        [Buffer.from(uiIx.name)],
-        new PublicKey(programId)
-      ))[0].toBase58(),
+    const ixId = (await PublicKey.findProgramAddress(
+      [Buffer.from(uiIx.name)],
+      new PublicKey(programId)
+    ))[0].toBase58();
+    let element = {
+      id: ixId,
       name: uiIx.name,
       label: uiIx.label,
-      accounts: [],
-      remainingAccounts: [],
-      args: []
-    } as Instruction;
+      help: uiIx.help,
+      type: uiIx.type,
+      value: uiIx.value,
+      visibility: uiIx.visibility,
+      dataElement: undefined      
+    } as UiElement;
     // accounts
-    let accIndex = 0;
-    for (let uiAcc of uiIx.accounts) {
-      let dataAcc = dataIx.accounts.filter((acc: any) => acc.name === uiAcc.name)[0];
-      if (dataAcc) {
-        instruction.accounts.push({
+    let accIndex = 0, argPosition = 0;
+    for (let uiElem of uiIx.uiElements) {
+      let dataElem = dataIx.accounts.filter((acc: any) => acc.name === uiElem.name)[0];
+      if (dataElem) {
+        element.dataElement = {
           index: accIndex,
-          label: uiAcc.label,
-          name: dataAcc.name,
-          isWritable: dataAcc.isMut,
-          isSigner: dataAcc.isSigner
-        } as Account);
+          name: dataElem.name,
+          isWritable: dataElem.isMut,
+          isSigner: dataElem.isSigner
+        } as Account;
         accIndex ++;
-      }
-    }
-    // args
-    let argPosition = 0;
-    for (let uiArg of uiIx.args) {
-      const dataArg = dataIx.args.filter((arg: any) => arg.name === uiArg.name)[0];
-      if (dataArg) {
-        const arg = {
+      } else {
+        dataElem = dataIx.args.filter((arg: any) => arg.name === uiElem.name)[0];
+        element.dataElement = {
           position: argPosition,
-          name: dataArg.name,
-          label: uiArg.label,
-          uiType: getUiType(uiArg.type),
-          dataType: dataArg.type
+          name: dataElem.name,
+          dataType: dataElem.type
         } as Arg;
-        instruction.args.push(arg);
         argPosition ++;
       }
     }
-    return instruction;  
+    return element;  
   } catch {
     return null;
   }
 };
-
-const getUiType = (type: UiType): string => {
-  if (
-    type === "Small" || 
-    type === "Number" || 
-    type === "Big" || 
-    type === "Float"
-  ) {
-    return "Number";
-  }
-  if (type === "String" || type === "PublicKey") {
-    return "String";
-  }
-  return `${type[0].toUpperCase()}${type.substring(1)}`;
-}
